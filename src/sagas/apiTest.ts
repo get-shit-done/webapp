856960aa5cb05a49f734fa39be3307584362b78e
vincent <vincentbollaert@gmail.com>
2020-07-08 12:43:40 +0700

    misc

--- src/sagas/apiTest.ts
+++ src/sagas/apiTest.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 function* fetchUser() {
    try {
       const response = yield call(axios.get, 'http://localhost:3333/login');
-      yield put({type: "USER_FETCH_SUCCEEDED", response});
+      yield put({type: "USER_FETCH_SUCCEEDED", test: response.data });
    } catch (e) {
       yield put({type: "USER_FETCH_FAILED", message: e.message});
    }
