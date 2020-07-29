213e00d94c4b68e3396e5279dac72930a6f919a2
vincentbollaert <vincentbollaert@gmail.com>
2020-07-29 11:29:31 +0700

    update default hours

--- src/pages/Home/Sidebar/Settings.tsx
+++ src/pages/Home/Sidebar/Settings.tsx
@@ -3,17 +3,25 @@ import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 import { TextField } from '../../../components/form'
 import Button from '../../../components/Button/component'
+import { useAppDispatch, AppState } from '../../../Application/Root'
+import { useSelector } from 'react-redux'
+import { actions } from '../../../reducers/settings'
 
 const Form = styled.form`
   margin-bottom: var(--size-md);
 `
 
 const Settings: FC = () => {
+  const dispatch = useAppDispatch()
+  const { defaultHoursFrom, defaultHoursTo } = useSelector((state: AppState) => state.settings)
   const { register, handleSubmit, errors } = useForm()
 
   const onSubmit = (data: any) => {
-    console.log(data)
-    console.log(errors)
+    const dataMapped = {
+      defaultHoursFrom: Number(data.defaultHoursFrom),
+      defaultHoursTo: Number(data.defaultHoursTo)
+    }
+    dispatch(actions.updateSettings(dataMapped))
   }
 
   return (
@@ -21,17 +29,19 @@ const Settings: FC = () => {
       <TextField
         theme="light"
         isInForm
-        name="hoursFrom"
+        defaultValue={defaultHoursFrom}
+        name="defaultHoursFrom"
         placeholder="hours from"
-        errorMessage={errors.hoursFrom?.type}
+        errorMessage={errors.defaultHoursFrom?.type}
         inputRef={register({ required: true, maxLength: 5 })}
       />
       <TextField
         theme="light"
         isInForm
-        name="hoursTo"
+        defaultValue={defaultHoursTo}
+        name="defaultHoursTo"
         placeholder="hours to"
-        errorMessage={errors.hoursTo?.type}
+        errorMessage={errors.defaultHoursTo?.type}
         inputRef={register({ required: true, maxLength: 5 })}
       />
       <Button type="submit">
