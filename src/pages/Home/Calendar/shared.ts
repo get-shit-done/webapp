6d4838e9888090527dd3b507a9ff4e80b4934f90
vincentbollaert <vincentbollaert@gmail.com>
2020-07-30 10:46:37 +0700

    extract determineTimeFromY fn

--- src/pages/Home/Calendar/shared.ts
+++ src/pages/Home/Calendar/shared.ts
@@ -3,3 +3,20 @@ export type CalendarFormValues = {
   from: number
   to: number
 }
+
+export const determineTimeFromY = ({
+  y,
+  hoursAxis,
+  ref,
+}: {
+  y: number
+  ref: React.MutableRefObject<any>
+  hoursAxis: number[]
+}) => {
+  const percentage = (y / ref.current.getBoundingClientRect().height) * 100
+  const hourMin = hoursAxis[0]
+  const hourMax = hoursAxis[hoursAxis.length - 1] + 1
+  const alg = hourMin + ((hourMax - hourMin) / 100) * percentage
+  const rounded = Math.round(alg / 0.25) * 0.25
+  return rounded
+}
