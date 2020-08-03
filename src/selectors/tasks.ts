ab15a5585b2b4cf787fae03490cbdc6828d85fc6
vincentbollaert <vincentbollaert@gmail.com>
2020-08-03 11:09:34 +0700

    add timestamp in tasks selector

--- src/selectors/tasks.ts
+++ src/selectors/tasks.ts
@@ -4,34 +4,37 @@ import { SavedTask } from '../reducers/calendarTasks'
 export const selectTasksMapped: any = createSelector(
   (state: any) => state.calendarTasks,
   (state: any) =>
-    state.daysAxis.map((datestring: string) => {
-      // console.log(datestring, state.allTasksByDay)
-      const tasks = state.allTasksByDay[datestring]?.tasks || []
-      return tasks.map((task: SavedTask, taskIndex: number) => {
-        const { _id, time, ...rest } = task
+    state.daysAxis.map((timestamp: string) => {
+      // console.log(timestamp, state.allTasksByDay)
+      const tasks = state.allTasksByDay[timestamp]?.tasks || []
+      return {
+        timestamp,
+        tasks: tasks.map((task: SavedTask, taskIndex: number) => {
+          const { _id, time, ...rest } = task
 
-        const from = time[0]
-        const to = time[1]
-        const isFirstTask = taskIndex === 0
-        const isLastTask = taskIndex === tasks.length - 1
-        const previousTo = !isFirstTask ? tasks[taskIndex - 1].time[1] : 0
-        // const firstHour = 0
-        // const lastHourAdjusted = 23
-        const firstHour = state.hoursAxis[0]
-        const lastHourAdjusted = state.hoursAxis[state.hoursAxis.length - 1] + 1
+          const from = time[0]
+          const to = time[1]
+          const isFirstTask = taskIndex === 0
+          const isLastTask = taskIndex === tasks.length - 1
+          const previousTo = !isFirstTask ? tasks[taskIndex - 1].time[1] : 0
+          // const firstHour = 0
+          // const lastHourAdjusted = 23
+          const firstHour = state.hoursAxis[0]
+          const lastHourAdjusted = state.hoursAxis[state.hoursAxis.length - 1] + 1
 
-        let heightInFlex = Math.min(to, lastHourAdjusted) - Math.max(from, firstHour)
-        let gapBefore = Math.min(from - previousTo, from - firstHour, lastHourAdjusted - previousTo)
-        let gapAfter = isLastTask ? lastHourAdjusted - to : 0
+          let heightInFlex = Math.min(to, lastHourAdjusted) - Math.max(from, firstHour)
+          let gapBefore = Math.min(from - previousTo, from - firstHour, lastHourAdjusted - previousTo)
+          let gapAfter = isLastTask ? lastHourAdjusted - to : 0
 
-        return {
-          _id,
-          heightInFlex,
-          gapBefore,
-          gapAfter,
-          time,
-          ...rest,
-        }
-      })
+          return {
+            _id,
+            heightInFlex,
+            gapBefore,
+            gapAfter,
+            time,
+            ...rest,
+          }
+        }),
+      }
     }),
 )
