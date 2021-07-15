5520983a9acc1d27297c18f2fd0e67eb9976bc19
Vince <vincent.bollaert@personio.de>
2021-07-15 14:32:15 +0200

    update api url for live

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,10 @@ import { actions } from "../reducers/calendar";
 
 import { actions as toastActions } from "../components/Toast/reducer";
 
-const URL = "http://localhost:3005/api/v1";
+const IS_DEV = false;
+const URL = IS_DEV
+  ? "http://localhost:3005/api/v1"
+  : "http://get-shit-done-tasks-api-env.eba-uvw6ykkw.eu-central-1.elasticbeanstalk.com/api/v1";
 
 export const API_TASKS = `${URL}/tasks`;
 export const API_TASKS_BY_ID = (id: string) => `${URL}/tasks/${id}`;
